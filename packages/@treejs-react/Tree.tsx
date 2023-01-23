3ba660f9304ce9ffed0278588c847bef1b74a130
付志强 <zhiqiangfu6@gmail.com>
2023-01-23 21:59:35 +0800

    fix: clear setup

--- packages/@treejs-react/Tree.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Tree() {
-    return (
-        <div>Tree</div>
-    )
-}
--- packages/@treejs-react/TreeNode.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function TreeNode() {
-    return (
-        <div>TreeNode</div>
-    )
-}
