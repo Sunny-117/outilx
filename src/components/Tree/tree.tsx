82169d67e1adc97a056cb38b6d97b17dc0a52553
付志强 <zhiqiangfu6@gmail.com>
2023-01-24 10:14:41 +0800

    feat: tree basisi

--- src/components/Tree/tree.tsx
+++ src/components/Tree/tree.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
-
-export default function Tree() {
+export default function Tree({ data, show = true, setShow }): JSX.Element {
     return (
-        <div>Tree</div>
+        <ul style={{
+            display: show ? 'block' : 'none'
+        }}>
+            {
+                Array.isArray(data) && data.length && data.map(item => {
+                    return <li key={item.value} onClick={(e) => {
+                        e.stopPropagation()
+                        setShow(item)
+                    }}>
+                        {item.value}
+                        {item.children && <Tree setShow={setShow} data={item.children} show={item.show}></Tree>}
+                    </li>
+                })
+            }
+        </ul>
     )
 }
