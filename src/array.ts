bce7cbb3a230d93c0c48f140654b39a5545a0b57
Sunny-117 <zhiqiangfu6@gmail.com>
2024-09-20 11:25:22 +0800

    pipFromArray

--- src/array.ts
+++ src/array.ts
@@ -1,4 +1,18 @@
-
 export function createIncrementingArray(length: number) {
-    return Array.from({ length }, (_, index) => index + 1);
+  return Array.from({ length }, (_, index) => index + 1);
+}
+
+/**
+ * 把一个函数的数组变成一个函数 compose
+ */
+export function pipeFromArray(fns: Function[]) {
+  if (fns.length === 0) {
+    return (x) => x;
+  }
+  if (fns.length === 1) {
+    return fns[0];
+  }
+  return function (input) {
+    return fns.reduce((prev, fn) => fn(prev), input);
+  };
 }
