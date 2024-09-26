e1dd01f014e2ac1146b37b2be7bf448c80a7b2f4
Sunny-117 <zhiqiangfu6@gmail.com>
2024-09-26 17:57:24 +0800

    build: optimize build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,25 +1,27 @@
-import { defineConfig } from "tsup";
-
-import {readDirectory} from './scripts/utils'
+import { defineConfig, Options } from "tsup";
+import { readDirectory } from './scripts/utils';
 import path from "path";
 
 const entry = readDirectory(path.resolve(__dirname, 'src/core'));
 
-export default defineConfig({
-  // entry: ["src/index.ts"],
+const commonConfig: Options = {
   entry,
-  outDir: "dist",
   clean: true,
-  format: ["esm", "cjs", "iife"],
   minify: false,
   sourcemap: true,
   dts: true,
   target: 'es2015',
-  // platform: 'node',
-  // esbuild: true,
-  // shims: true,
-  // keepNames: true,
-  // keepNames: true,
-  // keepNames: true,
-  // keepNames: true,
+};
+
+const createConfig = (format: Options['format'], outDir: string) => ({
+  ...commonConfig,
+  format,
+  outDir,
 });
+
+// 导出配置
+export default defineConfig([
+  createConfig("esm", "dist/esm"),
+  createConfig("cjs", "dist/cjs"),
+  createConfig("iife", "dist/iife"),
+]);
