60b3c82a17e45d1fa193e3d9bf3a7d17f961c2c5
Sunny-117 <zhiqiangfu6@gmail.com>
2024-12-14 21:13:56 +0800

    feat(array): toArray

--- src/core/array.ts
+++ src/core/array.ts
@@ -1,3 +1,17 @@
+export function toArray<T>(
+  val: T | T[] | null | undefined,
+  defaultValue?: T,
+): T[] {
+  if (Array.isArray(val)) {
+    return val
+  } else if (val == null) {
+    if (defaultValue) return [defaultValue]
+    return []
+  } else {
+    return [val]
+  }
+}
+
 export function createIncrementingArray(length: number) {
   return Array.from({ length }, (_, index) => index + 1);
 }
